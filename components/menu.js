9257b6388c0b3d5e07d134d3867fd0c6814473e5
Anthony_Dellavecchia <dev@anthonydellavecchia.com>
2022-02-23 16:59:27 -0600

    feat(menu): 💄 update the menu bar

--- components/menu.js
+++ components/menu.js
@@ -10,48 +10,83 @@ export default function Menu({children}) {
     const [isOpen,setIsOpen] = useState(false);
     const openMenu= ()=> setIsOpen(!isOpen);
 
-    return(
-            <header>
-                <nav className={styles.navbar}>
-                    <Link href='/'>
-                     <a>
-                         <Image src="/menu/anthony-logo.png" alt="Anthony Dellavecchia Logo" width={102} height={64}/>
-                     </a>
-                    </Link>
-                <ul className={isOpen === false ? 
-                        styles.navmenu : styles.navmenu +' '+ styles.active}>
+    return (
+        <header>
+            <nav className={`${styles.navbar}`}>
+                <Link href="/">
+                    <a className={styles.anthony}>
+                        <Image
+                            src="/menu/tw.svg"
+                            alt="Twilio Logo"
+                            width={50}
+                            height={25}
+                        />
+                        Anthony
+                    </a>
+                </Link>
+                <ul
+                    className={
+                        isOpen === false
+                            ? styles.navmenu
+                            : styles.navmenu + " " + styles.active
+                    }
+                >
                     <li className={styles.navitem}>
-                       <Link href='/'>
-                         <a className={isOpen === false ? 
-                                    styles.navlink : styles.navlink+' '+styles.active}
-                                    onClick={openMenu}>Home</a>
+                        <Link href="/">
+                            <a
+                                className={
+                                    isOpen === false
+                                        ? styles.navlink
+                                        : styles.navlink + " " + styles.active
+                                }
+                                onClick={openMenu}
+                            >
+                                Home
+                            </a>
                         </Link>
                     </li>
                     <li className={styles.navitem}>
-                        <Link href='/about'>
-                          <a className={isOpen === false ? 
-                                    styles.navlink : styles.navlink+' '+styles.active}
-                                    onClick={openMenu}>About</a>
+                        <Link href="/about">
+                            <a
+                                className={
+                                    isOpen === false
+                                        ? styles.navlink
+                                        : styles.navlink + " " + styles.active
+                                }
+                                onClick={openMenu}
+                            >
+                                About
+                            </a>
                         </Link>
                     </li>
                 </ul>
-                <Link href="https://twitter.com/anthonyjdella">
-                    <a className={`${styles.link} ${styles.twitter}`}>
-                        <FontAwesomeIcon icon={faTwitter}/>
-                        &nbsp;@AnthonyJDella
-                    </a>
-                </Link>
-                <button className={isOpen === false ? 
-                                    styles.hamburger : styles.hamburger+' '+styles.active}
-                                    onClick={openMenu}
-                                    >
+                <div>
+                    <Link href="https://anthonydellavecchia.com/">
+                        <a className={`${styles.link} ${styles.more}`}>
+                            See More
+                        </a>
+                    </Link>
+                    <Link href="https://twitter.com/anthonyjdella">
+                        <a className={`${styles.link} ${styles.twitter}`}>
+                            <FontAwesomeIcon icon={faTwitter} />
+                            &nbsp;@AnthonyJDella
+                        </a>
+                    </Link>
+                </div>
+                <button
+                    className={
+                        isOpen === false
+                            ? styles.hamburger
+                            : styles.hamburger + " " + styles.active
+                    }
+                    onClick={openMenu}
+                >
                     <span className={styles.bar}></span>
                     <span className={styles.bar}></span>
                     <span className={styles.bar}></span>
                 </button>
-                </nav>
+            </nav>
             {children}
-            </header>
-
-    )
+        </header>
+    );
 }
--- components/meta.js
+++ components/meta.js
@@ -3,14 +3,14 @@ import Head from 'next/head'
 export default function Meta() {
     return(
         <Head>
-            <title>Anthony in DevRel</title>
+            <title>Anthony&apos;s Twilio Interview</title>
             <meta name="description" content="Anthony Dellavecchia, the software engineer" />
             <link rel="shortcut icon" href="/images/flat-avatar-2022.svg" />
             <link rel="apple-touch-icon" sizes="180x180" href="/images/flat-avatar-2022.svg" />
             <link rel="icon" type="image/png" sizes="32x32" href="/images/flat-avatar-2022.svg"/>
             <link rel="icon" type="image/png" sizes="16x16" href="/images/flat-avatar-2022.svg"/>
 
-            <meta property="og:title" content="Anthony in DevRel" />
+            <meta property="og:title" content="Anthony's Twilio Interview" />
             <meta property="og:type" content="website" />
         </Head>
     )
